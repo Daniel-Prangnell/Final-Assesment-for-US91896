1ec20b229e3f08167bd7bb08f6184bb320940fb3
Daniel Prangnell <84354822+Daniel-Prangnell@users.noreply.github.com>
2022-07-27 23:35:40 +0000

    28/07/2022 11:35 Started added base outline added the welcoming sentence started doing the length

---
+++ main.py
@@ -0,0 +1,46 @@
+#import statements
+import os
+import time
+import math
+#functions
+def FindLength():
+  input("y")
+  
+
+#---------------Main Routine----------------
+
+#Date of Creation:28/07/2022
+#purpose: to welcome the user
+#version: 1.0
+#creator: Daniel Prangnell
+
+print("Welcome to this right angle triangle calculator V1.0 \n\nThis is designed to make your homework easier.")
+input("Press Enter to begin:")
+time.sleep(2) #Adds 2 second pause
+os.system('clear') #Clears screen
+Exit_Code = ""
+while Exit_Code != "xxx":
+  #Getting known length values
+  Length_1 = 0
+  Length_2 = 0
+  Length_3 = 0
+  FindLength()
+  
+  #Getting known aqngle values
+  Angle_1 = 0
+  Angle_2 = 0
+  Angle_3 = math.pi / 2 #90° angle
+  
+  #calculations (lengths)
+  
+  #Calculations (angles)
+  
+  #show user all values
+  
+  #store values in external txt file
+  
+  #repeat
+  break
+
+#show all previous values
+  
